b832064428f4460c37791ce3880856331210a417
Filips Barakats <philipe.barakat@yahoo.com>
2023-04-11 08:49:13 +0200

    updt

--- src/main/java/project/main/Main.java
+++ src/main/java/project/main/Main.java
@@ -1,5 +1,8 @@
 package project.main;
 
+import project.functions.ConsoleFunctions;
+import project.functions.SaveFunctions;
+
 import static project.classes.Color.returnColoredText;
 import static java.util.Arrays.stream;
 import static project.classes.Wizard.wizard;
@@ -9,18 +12,9 @@ import static project.functions.SaveFunctions.saveObject;
 
 public class Main {
     public static void main(String[] args) {
-        wizard.setExperience(10000);
-        wizard.updateStats();
-//        saveObject("test", wizard);
-        loadObject("test", wizard);
-
-//        System.out.println(findAllClasses("project/classes"));
-
-        System.out.println(wizard.getName());
-//        loadObject("test", Level.The_Chamber_of_Secrets);
-//        ConsoleFunctions.printMainScreen();
-//        SaveFunctions.checkSaves();
-//        ConsoleFunctions.chooseAction();
+        ConsoleFunctions.printMainScreen();
+        SaveFunctions.checkSaves();
+        ConsoleFunctions.chooseAction();
     }
 
 
